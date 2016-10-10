f9d6fb706d80d7eb0b1e87ea0213e802428e4adf
Eric Thomas <eric.l.m.thomas@gmail.com>
2016-10-10 14:32:30 -0600

    fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -63,7 +63,7 @@ config :logger, level: :info
 # import_config "prod.secret.exs"
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :editor, Editor.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
