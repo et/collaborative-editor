4ca0dc54467ec2bc7d7cd800e07c1e10972c8dfa
Eric Thomas <eric.l.m.thomas@gmail.com>
2016-10-18 16:22:04 -0600

    removed unused templates and tests

--- test/controllers/document_controller_test.exs
+++ test/controllers/document_controller_test.exs
@@ -10,22 +10,6 @@ defmodule Editor.DocumentControllerTest do
     assert html_response(conn, 200) =~ "Listing documents"
   end
 
-  test "renders form for new resources", %{conn: conn} do
-    conn = get conn, document_path(conn, :new)
-    assert html_response(conn, 200) =~ "New document"
-  end
-
-  test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, document_path(conn, :create), document: @valid_attrs
-    assert redirected_to(conn) == document_path(conn, :index)
-    assert Repo.get_by(Document, @valid_attrs)
-  end
-
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, document_path(conn, :create), document: @invalid_attrs
-    assert html_response(conn, 200) =~ "New document"
-  end
-
   test "shows chosen resource", %{conn: conn} do
     document = Repo.insert! %Document{}
     conn = get conn, document_path(conn, :show, document)
@@ -36,31 +20,5 @@ defmodule Editor.DocumentControllerTest do
     assert_error_sent 404, fn ->
       get conn, document_path(conn, :show, -1)
     end
-  end
-
-  test "renders form for editing chosen resource", %{conn: conn} do
-    document = Repo.insert! %Document{}
-    conn = get conn, document_path(conn, :edit, document)
-    assert html_response(conn, 200) =~ "Edit document"
-  end
-
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    document = Repo.insert! %Document{}
-    conn = put conn, document_path(conn, :update, document), document: @valid_attrs
-    assert redirected_to(conn) == document_path(conn, :show, document)
-    assert Repo.get_by(Document, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    document = Repo.insert! %Document{}
-    conn = put conn, document_path(conn, :update, document), document: @invalid_attrs
-    assert html_response(conn, 200) =~ "Edit document"
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    document = Repo.insert! %Document{}
-    conn = delete conn, document_path(conn, :delete, document)
-    assert redirected_to(conn) == document_path(conn, :index)
-    refute Repo.get(Document, document.id)
   end
 end
